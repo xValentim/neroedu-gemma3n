4c80f45a8cbea58b71bdbd51da265449ea26f712
HudsonArauj <hudsonma@al.insper.edu>
2025-07-31 11:42:28 -0300

    feat:simulado

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ from pydantic import BaseModel
 class InputData(BaseModel):
     essay: str = example_essay
     model_name: str = "gemma3n:e2b"
+    
+    
+class InputSimulado(BaseModel):
+    tema: str
+    model_name: str = "gemma3n:e2b"
 
 app = FastAPI()
 
@@ -92,5 +97,55 @@ async def call_model_competencia_1(input_data: InputData):
     
     return {"response": response}
 
+
+@app.post("/call-simulado")
+async def call_simulado(input_simulado: InputSimulado):
+    tema = input_simulado.tema
+    model_name = input_simulado.model_name
+    template = [
+        ('system', """Você irá gerar questões para compor um simulado do ENEM."""),
+        ('system', "Não crie questões unicamente objetivas, como 'O que é?', 'Quem foi?', contextualize breventemente o tema e crie questões que exijam raciocínio."),
+        ('system', """Siga a estrutura:
+            [
+                {{
+                "question": "string",
+                "A": "string",
+                "B": "string",
+                "C": "string",
+                "D": "string",
+                "E": "string",
+                "correct_answer": "A|B|C|D|E",
+                "explanation": "string"
+                }},
+                {{
+                "question": "string",
+                "A": "string",
+                "B": "string",
+                "C": "string",
+                "D": "string",
+                "E": "string",
+                "correct_answer": "A|B|C|   D|E",
+                "explanation": "string"
+                }},
+                ...
+            ]
+        """),
+        ('system', "Gere 5 questões sobre o tema: {tema}"),
+    ]
+
+    prompt = ChatPromptTemplate.from_messages(template)
+
+    llm = ChatOllama(model=model_name,
+                     temperature=0.0)
+
+    chain_simulado = (
+        prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    response = await chain_simulado.ainvoke({"tema": tema})
+    return response
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
