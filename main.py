89436c4904dc15d422e1c4dc1c67ab95a7563084
xValentim <joaogvr@al.insper.edu.br>
2025-08-02 19:39:20 -0300

    chore: add app in uvicorn

--- main.py
+++ main.py
@@ -345,4 +345,4 @@ async def call_key_topics(input_data_key_topics: InputDataKeyTopics):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
